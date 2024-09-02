84aa2af3bed54bf225a387693e67ee4e6b9c94c8
merytpeters <merytpeters@gmail.com>
2024-09-02 14:42:23 +0100

    Update admin.py

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Signup
 # Register your models here.
 
-# admin.site.register(Signup)
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(Signup)
+admin.site.register(Patient)
