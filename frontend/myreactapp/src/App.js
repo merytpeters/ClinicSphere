e869b032ed61ff77e6345acb9a4303dbd74d6d2d
merytpeters <merytpeters@gmail.com>
2024-09-02 15:19:46 +0100

    Update App.js

--- frontend/myreactapp/src/App.js
+++ frontend/myreactapp/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from react-router-dom
+import HomePage from './HomePage';
+import Login from './Login';
+import React from 'react';
+import Signup from './Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1> CLINICSPHERE </h1>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+         </Switch>
+       </main>
+      </div>
+    </Router>
   );
 }
 
