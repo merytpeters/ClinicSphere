0015079452160601d8865423d619de545749543f
merytpeters <merytpeters@gmail.com>
2024-09-03 15:55:08 +0100

    Pycodestyle errors fixed, django admin superuser created, can now assess views api

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from myapp.views import SignupViewSet, HomePageView
+from myapp.views import SignupViewSet, HomePageView, LoginView
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register(r'signups', SignupViewSet)
+router.register(r'signup', SignupViewSet)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='Homepage'),
-    path('api/login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path(r'', include(router.urls)),
 ]
