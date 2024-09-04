001f55e0abe6f7a0f06063b84236bf7c007608c6
merytpeters <merytpeters@gmail.com>
2024-09-04 10:38:11 +0100

    rearrange directory

--- frontend/myreactapp/src/services/auth.js
+++ frontend/myreactapp/src/services/auth.js
@@ -1,14 +1,19 @@
-// src/services/auth.js
-
 import api from './api'; // Import the axios instance
+
+export const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  return !!token; // Return true if token exists
+};
 
 export const login = async (username, password) => {
   try {
     const response = await api.post('/token/', { username, password });
-    localStorage.setItem('token', response.data.access); // Store JWT token
+    const token = response.data.access;
+    localStorage.setItem('token', token); // Store JWT token
     return response.data;
   } catch (error) {
-    throw error.response.data;
+    console.error("Login error:", error.response?.data || error.message);
+    throw error.response?.data || error.message;
   }
 };
 
