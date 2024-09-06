99d6205bdf28937ee7ea03a552970c4a41226678
merytpeters <merytpeters@gmail.com>
2024-09-06 16:58:43 +0100

    Updated the apis but the logic still needs to be refined

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,41 +1,40 @@
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views import View
-# from django.views.decorators.csrf import csrf_exempt
 from myapp.models import *
 from myapp.serializers import *
-from rest_framework import viewsets, status
+from myapp.permissions import *
+from rest_framework import generics, viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 
 
 # Create your views here.
-class HomePageView(View):
-    def get(self, request):
-        return HttpResponse("Welcome to homepage")
-
-
 class DepartmentsViewSet(viewsets.ModelViewSet):
     queryset = Departments.objects.all()
     serializer_class = DepartmentSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
-class EmployeesViewSet(viewsets.ModelViewSet):
+class EmployeesSignupView(generics.CreateAPIView):
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class SignupViewSet(viewsets.ModelViewSet):
     serializer_class = SignupSerializer
     queryset = Signup.objects.all()
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
 
-
-class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAuthenticated(), IsEmployee()]
+        return [IsAuthenticated()]
 
 
 class LoginView(APIView):
@@ -43,19 +42,23 @@ class LoginView(APIView):
         identifier = request.data.get('username_or_email')
         password = request.data.get('password')
 
-        try:
-            user_obj = User.objects.get(email=identifier)
-            username = user_obj.username
-        except User.DoesNotExist:
-            username = identifier
+        # try to get user by email, or username if email not found
+        user = None
+        if identifier:
+            try:
+                user = User.objects.get(email=identifier)
+            except User.DoesNotExist:
+                user = user.objects.filter(username=identifier).first()
 
-        user = authenticate(request, username=identifier, password=password)
-        if user is not None:
-            login(request, user)
-            return Response(
-                {"message": "Login successful"},
-                status=status.HTTP_200_OK
-            )
+        if user is not None and authenticate(
+            request, username=user.username, password=password
+        ):
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                "message": "Login successful"
+                }, status=status.HTTP_200_OK)
         else:
             return Response(
                 {"error": "Invalid Credentials"},
@@ -63,11 +66,22 @@ class LoginView(APIView):
             )
 
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
 class PatientViewSet(viewsets.ModelViewSet):
     serializer_class = PatientSerializer
     queryset = Patient.objects.all()
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class PatientFolderViewSet(viewsets.ModelViewSet):
-    seralizer_class = PatientFolderSerializer
+    serializer_class = PatientFolderSerializer
     queryset = PatientFolder.objects.all()
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
