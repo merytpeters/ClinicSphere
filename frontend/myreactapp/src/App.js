cf360c5d8aff024238087bc41ce053521a3ebc09
merytpeters <merytpeters@gmail.com>
2024-09-08 12:40:09 +0100

    Frontend landing page

--- frontend/myreactapp/src/App.js
+++ frontend/myreactapp/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
