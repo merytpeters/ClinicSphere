17d6c9910110ecd84a2f277d1960c8e66e48a77e
merytpeters <merytpeters@gmail.com>
2024-09-08 14:22:09 +0100

    Update settings.py
    Changed base dir to react frontend build directory

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
