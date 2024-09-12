c9041a64f7f6cd0fdd433f61988f5faab1804ce5
merytpeters <merytpeters@gmail.com>
2024-09-12 20:24:33 +0100

    Homepage

--- backend/frontend/myreactapp/src/App.js
+++ backend/frontend/myreactapp/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Home from './pages/Home'
 import Login from './pages/Login';
@@ -12,6 +12,11 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+            </ul>
+          </nav>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
