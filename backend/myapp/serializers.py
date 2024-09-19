4bff9d2064a75c89ae662597ca1a15fdb2f39c2e
merytpeters <merytpeters@gmail.com>
2024-09-19 21:33:57 +0100

    Rewrote seializers for employee registration, signup, login and logout

--- backend/myapp/serializers.py
+++ backend/myapp/serializers.py
@@ -1,98 +1,49 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate
 from myapp.models import *
 from rest_framework import serializers
 
 
-class DepartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Departments
-        fields = ('DepartmentName')
-
-
-class UserSerializer(serializers.ModelSerializer):
-    """Serializer for creating a User along with an Employee"""
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
-
-
 class EmployeeSerializer(serializers.ModelSerializer):
     """Serializer for creating an employee, including User creation"""
-    user = UserSerializer()
+    class Meta:
+        model = Employees
+        fields = ('id', 'username', 'email')
+
+class EmployeeRegistrationSerializer(serializers.ModelSerializer):
+    """Employee Registration"""
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = Employees
-        fields = ('user',
-                  'EmployeeName',
-                  'is_staff',
-                  'is_active',
-                  'last_login',
-                  'Department',
-                  'DateEmployed',
-                  'Employees_Title')
+        fields = ('id', 'username', 'email', 'password1', 'password2')
+        extra_kwargs = {'password':{'write_only':True}}
 
-    def create(self, validated_data):
-        # Extract user data from validated data
-        user_data = validated_data.pop('user')
-        # Create user instance
-        user = User.objects.create_user(
-            username=user_data['username'],
-            email=user_data['email'],
-            password=user_data['password']
-        )
+        def validate(self, attrs):
+            if attrs['password1'] != attrs['password2']:
+                raise serializers.ValidationError("Passwords do not match")
+            
+            password = attrs.get('password1', '')
+            if len(password) < 8:
+                raise serializers.ValidationError('Password must be at least 8 characters!')
+
+        def create(self, validated_data):
+        # Extract password data from validated data
+            password = validated_data.pop('password1')
+            validated_data.pop('password2')
+        
         # Create employee instance
-        employee = Employees.objects.create(user, **validated_data)
-        return employee
+            return Employees.objects.create_user(password=password, **validated_data)
 
 
-class SignupSerializer(serializers.ModelSerializer):
+class EmployeeLoginSerializer(serializers.Serializer):
+    """Login Serializer for Employees"""
+    email = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = Signup
-        fields = ['email', 'username', 'password', 'created_at']
-        read_only_fields = ['created_at']
-
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = Signup(**validated_data)
-        user.password = make_password(password)
-        user.save()
-        return user
-
-
-# class LoginSerializer(serializers.ModelSerializer):
-    # password = serializers.CharField(write_only=True)
-# class Meta:
-
-class PatientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Patient
-        fields = ['first_name',
-                  'last_name',
-                  'date_of_birth',
-                  'gender',
-                  'address',
-                  'phone_number',
-                  'email',
-                  'medical_record_number',
-                  'date_registered',
-                  'weight',
-                  'height',
-                  'blood_type',
-                  'genotype']
-
-        def validate(self, data):
-            if data.get('weight') and data['weight'] <= 0:
-                raise serializers.ValidationError(
-                    {"Weight must be a positive number"}
-                    )
-            if data.get('height') and data['height'] <= 0:
-                raise serializers.ValidationError(
-                    {"Height must be a positive number"}
-                    )
-            return data
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError('Incorect Login Credentials')
 
