563eeb50c5a1423f8f271d7b01a7cd3d6ef172c3
merytpeters <merytpeters@gmail.com>
2024-09-19 21:36:21 +0100

    Updated the views and urls according to serializers for employees, in views get, post responses defined

--- backend/patientportal/urls.py
+++ backend/patientportal/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from patientportal.views import *
-from rest_framework.routers import DefaultRouter
-
-
-router = DefaultRouter()
-router.register(r'patientprofile', UserProfileViewSet)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path(r'', include(router.urls)),
+    path('setpassword/', PasswordSetView.as_view(), name='setpassword'),
+    path('patientlogin/', UserLoginAPIView.as_view(), name='login'),
+    path('patientlogout/', UserLogoutAPIView.as_view(), name='logout'),
+    path("token/refresh/", TokenRefreshView.as_view(), name='token'),
 ]
