196aecce344bb9cbf03af2cff1baaae759d6bb63
merytpeters <merytpeters@gmail.com>
2024-09-19 21:36:21 +0100

    Updated the views and urls according to serializers for employees, in views get, post responses defined

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth import authenticate
 from myapp.models import *
 from myapp.serializers import *
 from myapp.permissions import *
-from rest_framework import generics, viewsets, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -27,65 +28,51 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-"""class DepartmentsViewSet(viewsets.ModelViewSet):
-    queryset = Departments.objects.all()
-    serializer_class = DepartmentSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
 
+class EmployeeRegistrationAPIView(GenericAPIView):
+    """Employee Registration Api"""
+    permission_classes = (AllowAny,)
+    serializer_class = EmployeeRegistrationSerializer
 
-class EmployeesSignupView(generics.CreateAPIView):
-    queryset = Employees.objects.all()
-    serializer_class = EmployeeSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data['tokens'] = {"refresh":str(token),
+                          "access": str(token.access_token)}
+        return Response(data, status= status.HTTP_201_CREATED)
+    
 
+class EmployeeLoginAPIView(GenericAPIView):
+    """Employee Login Api"""
+    permission_classes = (AllowAny,)
+    serializer_class = EmployeeLoginSerializer
 
-class SignupViewSet(viewsets.ModelViewSet):
-    serializer_class = SignupSerializer
-    queryset = Signup.objects.all()
-    authentication_classes = [JWTAuthentication]
-    # permission_classes = [IsAuthenticated]
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        serializer = EmployeeSerializer(user)
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data['tokens'] = {"refresh":str(token),
+                          "access": str(token.access_token)}
+        return Response(data, status= status.HTTP_201_CREATED)
+        
+        
+class EmployeeLogoutAPIView(GenericAPIView):
+    """Logout Api for employee"""
+    permission_classes = (IsAuthenticated,)
 
-    def get_permissions(self):
-        if self.request.method == 'POST':
-            return [IsAuthenticated(), IsEmployee()]
-        return [IsAuthenticated()]
-
-
-class LoginView(APIView):
-    def post(self, request):
-        identifier = request.data.get('username_or_email')
-        password = request.data.get('password')
-
-        # try to get user by email, or username if email not found
-        user = None
-        if identifier:
-            try:
-                user = User.objects.get(email=identifier)
-            except User.DoesNotExist:
-                user = user.objects.filter(username=identifier).first()
-
-        if user is not None and authenticate(
-            request, username=user.username, password=password
-        ):
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-                "message": "Login successful"
-                }, status=status.HTTP_200_OK)
-        else:
-            return Response(
-                {"error": "Invalid Credentials"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-
-
-class PatientViewSet(viewsets.ModelViewSet):
-    serializer_class = PatientSerializer
-    queryset = Patient.objects.all()
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]"""
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status= status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status= status.HTTP_400_BAD_REQUEST)
+        
 
