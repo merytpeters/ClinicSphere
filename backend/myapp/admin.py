c89bd75cd75afa6b2989605b6878662004d43b9c
merytpeters <merytpeters@gmail.com>
2024-09-21 10:29:40 +0100

    Prescriptionmedication, temporary token models registered

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -3,9 +3,11 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Departments)
+admin.site.register(TemporaryToken)
 admin.site.register(Employees)
 admin.site.register(Patient)
 admin.site.register(Medication)
 admin.site.register(Order)
 admin.site.register(Appointment)
 admin.site.register(Prescription)
+admin.site.register(PrescriptionMedication)
