7e7724a2508a97d0fe1514de309c6e7861c89bc2
merytpeters <merytpeters@gmail.com>
2024-09-22 23:34:30 +0100

    Landing page, about page, contact page and their styles done

---
+++ backend/frontend/myreactapp/src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Layout.css'
+
+const Contact = () => {
+    return (
+        <div className='body'>
+            <h2 className='title'>Connect with us:</h2>
+            <a href="https://github.com/merytpeters/ClinicSphere" target="_blank">GitHub</a><br>
+            </br>
+            <a href="https://www.linkedin.com/in/edafemerit" target="_blank">LinkedIn</a>
+        </div>
+    )
+};
+
+export default Contact;
