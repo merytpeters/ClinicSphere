6308c2c736ccea3aefca204f97c337c506a42661
merytpeters <merytpeters@gmail.com>
2025-06-17 16:11:37 +0100

    fix: Update template directory path for React app in settings

--- CS/backend/settings.py
+++ CS/backend/settings.py
@@ -114,7 +114,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'myreactapp', 'dist')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'myreactapp')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
